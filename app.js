c690fb7d53ca4caa9187d2504cf75c860f8d3148
MarshalJoe <joseph.charles.marshall@gmail.com>
2014-10-18 00:06:42 -0500

    addComment working

--- app.js
+++ app.js
@@ -64,5 +64,13 @@ app.controller('PostsCtrl', [
 'postFactory',
 function ($scope, $stateParams, postFactory) {
 	$scope.post = postFactory.posts[$stateParams.id];	
-
+	$scope.addComment = function () {
+		if ($scope.body === '') {return; }
+		$scope.post.comments.push({
+			body: $scope.body,
+			author: 'user',
+			upvotes: 0
+		})
+		$scope.body = '';
+	};
 }]);
