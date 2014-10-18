7551ebae03964ba0acf976a225cd28c52927b8cd
MarshalJoe <joseph.charles.marshall@gmail.com>
2014-10-17 19:59:08 -0500

    Hardcodes data into a new factory, rather than the controller

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 var app = angular.module('realtorNews', []);
 
+app.factory('postFactory', function() {
+	var factory = {}
+	factory.posts = [
+		{title: 'post 1', link:"https://www.facebook.com", upvotes: 5},
+		{title: 'post 2', link:"https://www.twitter.com", upvotes: 2}
+	]
+	return factory;
+});
+
 app.controller('MainCtrl', [
 '$scope',
-function ($scope) {
-	$scope.posts = [
-		{title: 'post 1', link:"https://www.facebook.com", upvotes: 5},
-		{title: 'post 2', link:"https://www.twitter.com", upvotes: 2},
-	];
+'postFactory',
+function ($scope, postFactory) {
+	$scope.posts = postFactory.posts;
 	$scope.addPost = function () {
 		if($scope.title === '') {return;}
 		$scope.posts.push({
