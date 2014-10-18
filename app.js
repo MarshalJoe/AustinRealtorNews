9ac38cc98c722175468eec987b65f3fe9fac12e4
MarshalJoe <joseph.charles.marshall@gmail.com>
2014-10-17 23:07:35 -0500

    Adds home state and template, doesn't remove controller tag

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-var app = angular.module('realtorNews', []);
+var app = angular.module('realtorNews', ['ui.router']);
+
+
+app.config([
+	'$stateProvider',
+	'$urlRouterProvider',
+	function ($stateProvider, $urlRouterProvider) {
+		$stateProvider
+			.state('home', {
+				url: '/home',
+				templateUrl: '/home.html',
+				controller: 'MainCtrl'
+			});
+
+		$urlRouterProvider.otherwise('home');
+	}]);
 
 app.factory('postFactory', function() {
 	var factory = {}
