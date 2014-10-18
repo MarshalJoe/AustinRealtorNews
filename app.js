b53e765c8fb16994fe61771b11b1b1194f98ebfa
MarshalJoe <joseph.charles.marshall@gmail.com>
2014-10-17 19:46:57 -0500

    Working link/upvote system

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-angular.module('realtorNews', [])
-.controller('MainCtrl', [
+var app = angular.module('realtorNews', []);
+
+app.controller('MainCtrl', [
 '$scope',
 function ($scope) {
-	$scope.test = 'Hello world';
 	$scope.posts = [
-		{title: 'post 1', upvotes: 5},
-		{title: 'post 2', upvotes: 2},
-		{title: 'post 3', upvotes: 15},
-		{title: 'post 4', upvotes: 9},
-		{title: 'post 5', upvotes: 4}	
+		{title: 'post 1', link:"https://www.facebook.com", upvotes: 5},
+		{title: 'post 2', link:"https://www.twitter.com", upvotes: 2},
 	];
 	$scope.addPost = function () {
 		if($scope.title === '') {return;}
-		$scope.posts.push({title: $scope.title, upvotes: 0});
+		$scope.posts.push({
+			title: $scope.title,
+			link: $scope.link, 
+			upvotes: 0});
 		$scope.title = '';
 	};
 	$scope.incrementUpvotes = function (post) {
